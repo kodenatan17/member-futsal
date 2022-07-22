8401bb0da3f66553a68fe1ca0568db5e74d8bf17
kodenatan17 <kodenatan17@gmail.com>
2022-07-22 13:58:15 +0700

    Adding CRUD Member

--- lib/app/modules/dashboard/views/dashboard_view.dart
+++ lib/app/modules/dashboard/views/dashboard_view.dart
@@ -2,23 +2,205 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../../constants.dart';
 import '../controllers/dashboard_controller.dart';
 
 class DashboardView extends GetView<DashboardController> {
-  const DashboardView({Key? key}) : super(key: key);
+  final c = Get.put(DashboardController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('DashboardView'),
-        centerTitle: true,
+      body: Container(
+        padding: const EdgeInsets.all(kDefaultPadding),
+        width: Get.width,
+        height: Get.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/bg.png"),
+            fit: BoxFit.cover,
+            opacity: 0.1,
+          ),
+        ),
+        child: Column(
+          children: [
+            Text(
+              "Selamat Datang",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "List ini adalah Member Tetap",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "di Football Membership!",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: kDefaultPadding,
+            ),
+            Obx(
+              () => Expanded(
+                child: ListView.builder(
+                  itemCount: c.menus.length,
+                  itemBuilder: (context, index) => Card(
+                    color: Colors.white,
+                    child: ListTile(
+                      title: Text(c.menus[index].name!),
+                      subtitle: Text(c.menus[index].phone!),
+                      leading: CircleAvatar(
+                        child: Text(
+                          c.menus[index].name!.substring(0, 1).capitalize!,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        backgroundColor: kDefaultColor,
+                      ),
+                      trailing: IconButton(
+                        onPressed: () {
+                          displayDeleteDialog(c.menus[index].docId!);
+                        },
+                        icon: Icon(
+                          Icons.delete_forever,
+                          color: Colors.red,
+                        ),
+                      ),
+                      onTap: () {
+                        c.nameC.text = c.menus[index].name!;
+                        c.phoneC.text = c.menus[index].phone!;
+                        _buildAddMenuView(
+                          text: 'UPDATE',
+                          addEditFlag: 2,
+                          docId: c.menus[index].docId!,
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-      body: Center(
-        child: Text(
-          'DashboardView is working',
-          style: TextStyle(fontSize: 20),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _buildAddMenuView(text: 'ADD', addEditFlag: 1, docId: '');
+        },
+        backgroundColor: kDefaultColor,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  _buildAddMenuView(
+      {required String text, required int addEditFlag, required String docId}) {
+    final controller = Get.put(DashboardController());
+    Get.bottomSheet(
+      Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(kDefaultCircular),
+            topLeft: Radius.circular(kDefaultCircular),
+          ),
+          color: kBgAccentColor,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(
+            16,
+            16,
+            16,
+            16,
+          ),
+          child: Form(
+            key: controller.formKey,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '${text} Member',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: kDefaultPadding,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.auto_fix_normal_outlined),
+                      hintText: 'Nama Member',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(kDefaultCircular),
+                      ),
+                    ),
+                    controller: controller.nameC,
+                    validator: (value) {
+                      return controller.validateName(value!);
+                    },
+                  ),
+                  SizedBox(
+                    height: kDefaultPadding,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.auto_fix_normal_outlined),
+                      hintText: 'Handphone Member',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(kDefaultCircular),
+                      ),
+                    ),
+                    controller: controller.phoneC,
+                    validator: (value) {
+                      return controller.validatePhone(value!);
+                    },
+                  ),
+                  ConstrainedBox(
+                    constraints: BoxConstraints.tightFor(
+                      width: Get.context!.width,
+                      height: 40,
+                    ),
+                    child: ElevatedButton(
+                      child: Text(
+                        text!,
+                        style: TextStyle(color: Colors.black, fontSize: 16),
+                      ),
+                      onPressed: () {
+                        controller.saveUpdateMenu(
+                          controller.nameC.text,
+                          controller.phoneC.text,
+                          docId!,
+                          addEditFlag!,
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
+  }
+
+  displayDeleteDialog(String docId) {
+    Get.defaultDialog(
+        title: "Delete Menu",
+        titleStyle: TextStyle(fontSize: 18),
+        middleText: "Apakah kamu yakin hapus data menu?",
+        textCancel: "Cancel",
+        textConfirm: "Confirm",
+        confirmTextColor: Colors.black,
+        onCancel: () {},
+        onConfirm: () {
+          controller.deleteData(docId);
+        });
   }
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,24 +1,82 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:member_futsal/app/modules/contact/views/contact_view.dart';
+import 'package:member_futsal/app/modules/dashboard/views/dashboard_view.dart';
+import 'package:member_futsal/app/modules/fnb/views/fnb_view.dart';
+import 'package:member_futsal/constants.dart';
 
+import '../../../controllers/auth_controller.dart';
+import '../../login/views/login_view.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+  Widget build(BuildContext context) => DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.notifications_active_rounded,
+                  color: kDefaultColor,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  AuthController().signOut().then(
+                        (_) => Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => LoginView(),
+                          ),
+                        ),
+                      );
+                },
+                icon: Icon(
+                  Icons.person_outline_outlined,
+                  color: kDefaultColor,
+                ),
+              ),
+            ],
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black87,
+            title: Container(
+              alignment: Alignment.centerLeft,
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text('Hi, Welcome!'),
+                ],
+              ),
+            ),
+            bottom: TabBar(
+              labelColor: Colors.black87,
+              indicatorColor: kDefaultColor,
+              indicatorWeight: 5,
+              tabs: [
+                Tab(
+                  text: 'Member',
+                ),
+                Tab(
+                  text: 'FnB',
+                ),
+                Tab(
+                  text: 'Contact',
+                )
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              DashboardView(),
+              FnbView(),
+              ContactView(),
+            ],
+          ),
         ),
-      ),
-    );
-  }
+      );
 }
